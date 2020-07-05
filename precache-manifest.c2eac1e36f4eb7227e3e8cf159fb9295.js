d05f00abeb2f4052d5c4ae04db4b2c5ee7236ac3
David Chao <dave56784@gmail.com>
2020-07-05 01:41:50 -0400

    Updates

---
+++ precache-manifest.c2eac1e36f4eb7227e3e8cf159fb9295.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af636cd5abc3b90db5630d1e8cede5a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b39390de0c202514ab50",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b44284d3cc762047781",
+    "url": "/portfolio/static/js/2.c518f652.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.c518f652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39390de0c202514ab50",
+    "url": "/portfolio/static/js/main.52762536.chunk.js"
+  },
+  {
+    "revision": "d8571477b91485592c76",
+    "url": "/portfolio/static/js/runtime-main.d46a18c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
